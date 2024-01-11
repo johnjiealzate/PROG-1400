4b18ce92346e5b12b62c9bde6be4540da87ba7e2
Johnjie Alzate <theflickpoet@gmail.com>
2024-01-11 10:56:25 -0400

    Update

--- Test.py
+++ Test.py
@@ -1,1 +1,20 @@
-# Constructor method
+# Parent class
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def  speak(self):
+        pass
+
+# Child class
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} says Woof!"
+    
+class CAt(Animal):
+    def speak(self):
+        return f"{self.name} says Meow!"
+    
+# Creating Objects
+dog = Dog("Buddy")
+cat = Cat("")
