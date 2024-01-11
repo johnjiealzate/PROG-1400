9972686bc520071ac383646c6d38d5ef71e072d9
Johnjie Alzate <theflickpoet@gmail.com>
2024-01-11 11:18:00 -0400

    Update

--- Inheritance.py
+++ Inheritance.py
@@ -1,7 +1,8 @@
 # Parent class
 class Animal:
-    def __init__(self, name):
+    def __init__(self, name, habitat):
         self.name = name
+        self.habitat = habitat
 
     def  speak(self):
         pass
@@ -14,7 +15,15 @@ class Dog(Animal):
 class Cat(Animal):
     def speak(self):
         return f"{self.name} says Meow!"
-    
+
+'''
+Objects are instances of the class.
+Here, dogs and cats are instances of the Dog and Cat sub-classes
+''' 
 # Creating Objects
-dog = Dog("Buddy")
-cat = Cat("Whiskers")
+dog = Dog("Buddy", "domestic")
+cat = Cat("Whisker", "domestic")
+
+# Using methods
+print(dog.speak())
+print(cat.speak())
