64d61aac9e2e6edacca4aa33a5169002ce51d619
Johnjie Alzate <theflickpoet@gmail.com>
2024-02-22 12:29:47 -0400

    code update

--- W0474627_PROG1400_OOP_Student Management System.py
+++ W0474627_PROG1400_OOP_Student Management System.py
@@ -7,10 +7,21 @@ class Student:
         self.grade = grade
 
     # Implement appropriate methods within the `Student` class for the following tasks.
-    def display_info(self, display_info):
+    def display_info(self):
+        return f"Student Information: {self.student_id} {self.student_id} {self.name} {self.age}"
 
 # Define a subclass called `GraduateStudent`, inheriting from the `Student` class. Add an additional attribute.
 class SraduateStudent(Student):
     def __init__(self, student_id, name, age, grade, thesis_topic):
         super().__init__(student_id, name, age, grade)
         self.thesis_topic = thesis_topic
+
+
+def search_student_by_id(self, student_id):
+     print ("Search student:")
+    for s in self.students:
+        if student.student_id == student_id:
+            print ("Student found")
+            student.display_info()
+            return
+        print("Student not found")
