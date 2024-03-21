b96de1d4b10938abe3aef40c1794d38b22fc64e9
Johnjie Alzate <theflickpoet@gmail.com>
2024-03-21 12:34:27 -0300

    Update Code

--- W0474627_Open_Weather_App.py
+++ W0474627_Open_Weather_App.py
@@ -6,14 +6,14 @@ class OpenMapAPI:
         self.api_key = api_key
         self.base_url = "https://api.openweathermap.org/data/2.5/weather"
 
-    def get_waether_by_city (self, city_name):
+    def get_weather_by_city(self, city_name):
         # https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-        url = f"{self.base_url}?q={city_name}&appif={self.api_key}"
+        url = f"{self.base_url}?q={city_name}&appid={self.api_key}"
         response = requests.get(url)
         # Error checking
         if response.status_code == 200:
-            data = response.json
+            data = response.json()
             return data
         else:
-            print(f"Check City Name, API Key, Network Connecetion, or API Service is down.")
+            print(f"Check City Name, API Key, Network Connection, or if API Service is down.")
             return None
--- W0474627_Open_Weather_App_V1.py
+++ W0474627_Open_Weather_App_V1.py
@@ -1,4 +1,14 @@
 import W0474627_Open_Weather_App
 # Enter your API Key
-api_key = "82005d27a116c2880c8f0fcb866998a0"
-open_map = W0474627_Open_Weather_App(api_key)
+api_key = "7183f487bd1c25233fb7f02cbb25f185"
+open_map = W0474627_Open_Weather_App.OpenMapAPI(api_key)
+KELVIN = 273.15
+# Prompt user for city
+city_name = input("Enter city name: ")
+city_weather = open_map.get_weather_by_city(city_name)
+if city_weather:
+    print("Weather in: ", city_name)
+    print("Description: ", city_weather['weather'][0]['description'])
+    print("Temperature: ", round(KELVIN - city_weather['main']['temp']), "degC")
+else:
+    print(f"Check City Name, API Key, Network Connection, or if API Service is down.")
