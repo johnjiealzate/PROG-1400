8dee1ff60d61bacf98310364e2a670b09c0049b8
Johnjie Alzate <theflickpoet@gmail.com>
2024-03-21 11:56:28 -0300

    New Assignments

---
+++ W0474627_Open_Weather_App.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+class OpenMapAPI:
+    def __init__(self, api_key) -> None:
+        self.api_key = api_key
+        self.base_url = "https://api.openweathermap.org/data/2.5/weather"
+
+    def get_waether_by_city (self, city_name):
+        # https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+        url = f"{self.base_url}?q={city_name}&appif={self.api_key}"
+        response = requests.get(url)
+        # Error checking
+        if response.status_code == 200:
+            data = response.json
+            return data
+        else:
+            print(f"Check City Name, API Key, Network Connecetion, or API Service is down.")
+            return None
---
+++ W0474627_Open_Weather_App_V1.py
@@ -0,0 +1,4 @@
+import W0474627_Open_Weather_App
+# Enter your API Key
+api_key = "82005d27a116c2880c8f0fcb866998a0"
+open_map = W0474627_Open_Weather_App(api_key)
